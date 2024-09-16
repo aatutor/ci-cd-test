438edce248847224059ed5a6269905626782e15b
Alexandr Timofeev <144218346+aatutor@users.noreply.github.com>
2024-09-16 11:12:36 +0700

    rm node

--- app.js
+++
@@ -1,9 +0,0 @@
-var app = require('express')();
-
-app.get('/', function (req, res) {
-	res.sendFile(__dirname + '/index.html');
-});
-
-app.listen(3000, function () {
-	console.log('Example app listening on port 3000!');
-});
